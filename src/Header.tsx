c1a59889de230ca134c2e1d71f1670459ebc6b4c
Vlad Stoenescu <64587367+vladstoe@users.noreply.github.com>
2024-05-17 13:19:21 +0200

    added hamburger menu for phone

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './assets/logo.jpg'; // Assuming the logo is stored here
 import styles from './Header.module.css';
+import { FaBars } from 'react-icons/fa'; // You need to install react-icons if you haven't
 
 interface HeaderProps {
     scrollTo: (id: string) => void;
 }
 
 const Header: React.FC<HeaderProps> = ({ scrollTo }) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <div className={styles.header}>
             <div className={styles.headerLeft}>
                 <img src={logo} alt="Logo" className={styles.logo} />
                 <span>ArtVista</span>
             </div>
-            <div className={styles.headerRight}>
-                <button onClick={() => scrollTo("app")}>App</button>
-                <button onClick={() => scrollTo("api")}>API</button>
-                <button onClick={() => scrollTo("roadmap")}>Roadmap</button>
-                <button onClick={() => scrollTo("updates")}>Updates</button>
-                <button onClick={() => scrollTo("aboutcontact")}>About & Contact Us</button>
+            <div className={styles.menuIcon} onClick={toggleMenu}>
+                <FaBars />
+            </div>
+            <div className={`${styles.headerRight} ${isMenuOpen ? styles.menuOpen : ''}`}>
+                <button onClick={() => { scrollTo("app"); toggleMenu(); }}>App</button>
+                <button onClick={() => { scrollTo("api"); toggleMenu(); }}>API</button>
+                <button onClick={() => { scrollTo("roadmap"); toggleMenu(); }}>Roadmap</button>
+                <button onClick={() => { scrollTo("updates"); toggleMenu(); }}>Updates</button>
+                <button onClick={() => { scrollTo("aboutcontact"); toggleMenu(); }}>About & Contact Us</button>
             </div>
         </div>
     );
